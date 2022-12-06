357fff62b147a10291550fd6f15ef5154f33f36c
Terahmumuli <terahmumuli@gmail.com>
2022-12-06 10:41:05 +0300

    created the different routes as well as endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :foods, only: [:index, :show, :create]
+  resources :carts, only: [:create, :destroy]
+  resources :user_foods, only: [:index, :create]
+
+  # post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
